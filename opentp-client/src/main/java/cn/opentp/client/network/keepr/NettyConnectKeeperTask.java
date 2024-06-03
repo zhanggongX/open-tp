17ffb54a74063811c27c4d7135ead849c6079a74
zhanggong <zhanggong@58.com>
2024-06-03 14:15:32 +0800

    守护线程

--- opentp-client/src/main/java/cn/opentp/client/network/keepr/NettyConnectKeeperTask.java
+++ opentp-client/src/main/java/cn/opentp/client/network/keepr/NettyConnectKeeperTask.java
@@ -6,7 +6,6 @@ import cn.opentp.client.network.ThreadPoolReportService;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
